645ce5a82f446860645b60b01a15b17a8bbfabaf
Fernando <fer.castillo.rdz999@gmail.com>
2023-09-20 20:12:00 -0600

    Se actualiza Proyecto

--- js/app.js
+++ js/app.js
@@ -1,108 +1,123 @@
-// Apartado para variables y constantes globales
-const users = document.getElementById('users');
+//Apartado para variables y constantes globales
+const users = document.getElementById('users')
 const templateUser = document.getElementById('template-user').content
-const fragment = document.createDocumentFragment() 
+const fragment = document.createDocumentFragment()
 const btnSave = document.getElementById('btnSave')
-const btnDelete = document.getElementById('btnDelete')
-let idUpdate = null;
-
-// Eventos de mi pagina
-
-document.addEventListener('DOMContentLoaded', () => {
-    loadData();
-});
-
-btnSave.addEventListener('click', (e) => {
-    e.preventDefault();
-    sendData();
+let idUpdate = null
+//Eventos de mi pagina 
+document.addEventListener('DOMContentLoaded',() => {
+    loadData()
 })
 
+btnSave.addEventListener('click', (e) => {
+    e.preventDefault()
+    sendData()
+})
+
+
 const sendData = () => {
-    const firstname = document.getElementById('firstname').value;
-    const lastname = document.getElementById('lastname').value;
-    const address = document.getElementById('address').value;
-    const phone = document.getElementById('phone').value;
-    const city = document.getElementById('city').value;
-    const zipcode = document.getElementById('zipcode').value;
-    //.trim => quita todos los espacios en blanco
-    if(firstname.trim().lengh !== 0 && lastname.trim().lengh !== 0){
+    const firstname = document.getElementById('firstname').value
+    const lastname = document.getElementById('lastname').value
+    const address = document.getElementById('address').value
+    const phone = document.getElementById('phone').value
+    const city = document.getElementById('city').value
+    const cp = document.getElementById('zipcode').value
+
+    if(firstname.trim().lenght !== 0 && lastname.trim().lenght !== 0) {
         const obj = {
             firstname,
             lastname,
             address,
             phone,
             city,
-            zipcode
+            cp
         }
-        fetch('http://localhost:5000/create', {
+
+        fetch('http://localhost:9000/create', {
             method: 'POST',
-            //.stringify => convierte un objeto en formato JSON
             body: JSON.stringify(obj),
             headers: {
-                'Content-type': "application/json; charset=UTF-8"
+                'Content-type': 'application/json; charset=UTF-8'
             }
-        }).then( async (res) => {
-            const result = await res.json();
-            if (result.msg == "success") {
-                loadData();
+        }).then(async(res) => {
+            const result = await res.json()
+            if(result.msg === 'success') {
+                loadData()
             }
-            console.log("@@@ result => ", result)
-        }).catch((res) => {
-            console.log("@@@ error => ", error)
+            console.log('$$ result => ', result)
+        }).catch((error) => {
+            console.log('$$error => ', error)
         })
     }
 }
-
 const loadData = async() => {
-    const data = await fetch('http://localhost:5000/');
-    const usuarios = await data.json();
-    //console.log('@@@ data => ', usuarios);
-    if (usuarios.msg === 'success'){
-        drawUsers(usuarios.data);
-        //console.log('@@@ data => ', usuarios.data);
+    const data = await fetch('http://localhost:9000/')
+    const usuarios = await data.json()
+    if (usuarios.msg == 'success'){
+        drawUsers(usuarios.data)
     }
 }
+
+
 
 const drawUsers = (items) => {
-    // .innerHTML = 
-    users.innerHTML = '';
-    items.forEach(function(user) {
-        console.log('@@@ user => ', user);
-        const clone = templateUser.cloneNode(true);
-        clone.querySelectorAll('th')[0].textContent = user.firstname;
-        clone.querySelectorAll('th')[1].textContent = user.lastname;
-        clone.querySelectorAll('th')[2].textContent = user.address;
-        clone.querySelectorAll('th')[3].textContent = user.phone;
-        clone.querySelectorAll('th')[4].textContent = user.city;
-        clone.querySelectorAll('th')[5].textContent = user.zipcode;
-        clone.querySelector('.btn-danger').dataset.id = user.id;
-        clone.querySelector('.btn-warning').dataset.id = user.id;
+    users.innerHTML = ''
+    items.forEach((user) => {
+        const clone = templateUser.cloneNode(true)
+        clone.querySelectorAll('th')[0].textContent = user.firstname
+        clone.querySelectorAll('th')[1].textContent = user.lastname
+        clone.querySelectorAll('th')[2].textContent = user.address
+        clone.querySelectorAll('th')[3].textContent = user.phone
+        clone.querySelectorAll('th')[4].textContent = user.city
+        clone.querySelectorAll('th')[5].textContent = user.cp
+        clone.querySelector('.btn-danger').dataset.id = user.id
+        clone.querySelector('.btn-warning').dataset.id = user.id
 
-        const btnDelete = clone.querySelector('.btn-danger');
+        //crear evento para borrar
+        const btnDelete = clone.querySelector('.btn-danger')
         btnDelete.addEventListener('click', () => {
-            //console.log('@@@ espero => ', btnDelete.dataset.id)
+            console.log('$$ espero funcione => ', btnDelete.dataset.id)
             deleteUser(btnDelete.dataset.id)
-        })
+            window.location.replace('/')
+        }) 
 
-        // Crear btn para actualizar
-        const btnUpdate = clone.querySelector('.btn-warning');
+         // Crear btn para actualizar
+        // const btnUpdate = clone.querySelector('.btn-warning');
+        // btnUpdate.addEventListener('click', () => {
+        //     idUpdate = btnUpdate.dataset.id;
+             //window.location.replace(`/update-user.html?id=${idUpdate}`);
+        //     window.location = (`http://localhost:5050/update-user.html?id=${idUpdate}`);
+
+         //crear evento para actualizar
+        const btnUpdate = clone.querySelector('.btn-warning')
         btnUpdate.addEventListener('click', () => {
-            idUpdate = btnUpdate.dataset.id;
-            //window.location.replace(`/update-user.html?id=${idUpdate}`);
-            window.location = (`http://localhost:5050/update-user.html?id=${idUpdate}`);
+            idUpdate = btnUpdate.dataset.id
+            window.location.replace(`/update-user.html?id=${idUpdate}`)
         })
 
-        fragment.appendChild(clone);
+        
+        fragment.appendChild(clone)
     })
-    users.appendChild(fragment);
+    users.appendChild(fragment)
 }
+
 
 const deleteUser = async (id) => {
-    const res = await fetch(`http://localhost:5000/delete/${id}`);
-    const result = await res.json();
-    console.log('@@@ result => ', result);
-    if (result.msg === 'user deleted'){
-        loadData();
+    console.log('$$$ id => ', id)
+    const res = await fetch(`http://localhost:9000/delete/${id}`)
+    const result = await res.json()
+    if (result.msg === 'user deleted') {
+        loadData()
     }
-
+    //console.log('$$ result => ', result)
 }
+
+//const deleteUser = async (id) => {
+//    const res = await fetch(`http://localhost:5000/delete/${id}`);
+//    const result = await res.json();
+//    console.log('@@@ result => ', result);
+//    if (result.msg === 'user deleted'){
+//        loadData();
+//    }
+
+//}
--- js/update.js
+++ js/update.js
@@ -1,23 +1,23 @@
-const btnUpdate = document.getElementById('btnUpdate');
+const btnUpdate = document.getElementById('btnUpdate')
 
-let idUpdate;
-let id;
+let idUpdate
+let id
 document.addEventListener('DOMContentLoaded', () => {
-    idUpdate = location.search.substring(1).split("&");
-    id = idUpdate[0].substring(3, idUpdate[0].length);
-    getUser(id);
-    //console.log('@@@ idUpdate => ', idUpdate, id);
+    idUpdate = location.search.substring(1).split("&")
+    id = idUpdate[0].substring(3, idUpdate[0].length)
+    getUser(id)
+    console.log('$$ idUpdate => ', idUpdate)
 })
 
 btnUpdate.addEventListener('click', (e) => {
-    const firstname = document.getElementById('firstname').value;
-    const lastname = document.getElementById('lastname').value;
-    const address = document.getElementById('address').value;
-    const phone = document.getElementById('phone').value;
-    const city = document.getElementById('city').value;
-    const zipcode = document.getElementById('zipcode').value;
-    //.trim => quita todos los espacios en blanco
-    if(firstname.trim().lengh !== 0 && lastname.trim().lengh !== 0){
+    const firstname = document.getElementById('firstname').value
+    const lastname = document.getElementById('lastname').value
+    const address = document.getElementById('address').value
+    const phone = document.getElementById('phone').value
+    const city = document.getElementById('city').value
+    const cp = document.getElementById('zipcode').value
+
+    if(firstname.trim().lenght !== 0 && lastname.trim().lenght !== 0) {
         const obj = {
             id,
             firstname,
@@ -25,47 +25,49 @@ btnUpdate.addEventListener('click', (e) => {
             address,
             phone,
             city,
-            zipcode
+            cp
         }
-        fetch('http://localhost:5000/update', {
+
+        fetch('http://localhost:9000/update', {
             method: 'POST',
-            //.stringify => convierte un objeto en formato JSON
             body: JSON.stringify(obj),
             headers: {
-                'Content-type': "application/json; charset=UTF-8"
+                'Content-type': 'application/json; charset=UTF-8'
             }
-        }).then( async (res) => {
-            const result = await res.json();
-            if (result.msg == "success") {
-                window.location.replace('/');
+        }).then(async(res) => {
+            const result = await res.json()
+            if(result.msg === 'success'){
+                window.location.replace('/')
             }
-            console.log("@@@ result => ", res)
-        }).catch((res) => {
-            console.log("@@@ error => ", error)
+            console.log('$$ result => ', res)
+        }).catch((error) => {
+            console.log('$$error => ', error)
         })
     }
     e.preventDefault()
 })
 
+
+
 const getUser = async (id) => {
-    const user = await fetch(`http://localhost:5000/get-update/${id}`);
-    const result = await user.json();
-    //console.log('@@@ resul => ', result)
-    if (result.msg === "success"){
-        loadDataUser(result.data);
+    const user = await fetch(`http://localhost:9000/get-update/${id}`)
+    const result = await user.json()
+    if (result.msg === 'success') {
+        loadDataUser(result.data)
     } else {
-        alert('User Not Fund');
+        alert('User not found')
     }
+    console.log('$$result => ', result)
 }
 
 const loadDataUser = (user) => {
-    const id = document.getElementById ('id');
-    const firstname = document.getElementById ('firstname');
-    const lastname = document.getElementById ('lastname');
-    const address = document.getElementById ('address');
-    const phone = document.getElementById ('phone');
-    const city = document.getElementById ('city');
-    const zipcode = document.getElementById ('zipcode');
+    const id = document.getElementById('id')
+    const firstname = document.getElementById('firstname')
+    const lastname = document.getElementById('lastname')
+    const address = document.getElementById('address')
+    const phone = document.getElementById('phone')
+    const city = document.getElementById('city')
+    const zipcode = document.getElementById('zipcode')
 
     id.value = id
     firstname.value = user.firstname
